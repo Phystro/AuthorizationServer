cd140a490b36b3d2cae64dbe0a5650b7eb7eecb3
Phystro <phystroq@gmail.com>
2023-07-15 03:18:12 +0300

    add: enable authentication

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); // configure MVC
+
+// Cookie Authentication - register Cookir Authentication scheme
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.LoginPath = "/account/login";
+            });
 
 var app = builder.Build();
 
@@ -12,7 +21,12 @@ if (app.Environment.IsDevelopment())
 // enable serving of static files, to serve our style sheets out of the wwwroot folder
 app.UseStaticFiles();
 
+// Make route information available for authentication decisions. Authentication comes
+// before EndPoints so that users are authenticated before accessing the endpoints
 app.UseRouting();
+
+// authentcation middleware, uses registered authentcation schemes
+app.UseAuthentication();
 
 // Endpoints are setup to use default routing
 app.UseEndpoints(endpoints =>
@@ -20,6 +34,6 @@ app.UseEndpoints(endpoints =>
         endpoints.MapDefaultControllerRoute();
         });
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
 
 app.Run();
